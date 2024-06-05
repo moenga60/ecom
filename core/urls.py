8547cf7b4badb7c1b25b71d22d957005b728ee98
moenga60 <moengadaniel60@gmail.com>
2024-06-05 09:54:03 +0300

    templates

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from core.views import index
 # from core import views
-
+# from core.views import shop
 
 app_name = "core"
 
 urlpatterns = [
     path("", index)
     # path("", views.core)
+    
 ]
+
+# urlpatterns = [
+#     path("/templates/core/shop.html", shop)
+# ]
